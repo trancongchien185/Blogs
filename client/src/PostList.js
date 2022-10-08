b6b52ccf4119ed75b5bc55cfdfda532b12eb6bee
trancongchien <trancongchien185@gmail.com>
2022-10-08 21:54:22 +0700

    Displaying Comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
     const [posts, setPosts] = useState({});
@@ -24,14 +25,18 @@ const PostList = () => {
         >
             <div className="card-body">
                 <h3>{post.title}</h3>
+                <CommentList postId={post.id} />
                 <CommentCreate postId={post.id} />
+
             </div>
         </div>
         );
     });
 
-    return <div className="d-flex flex-row flex-wrap justify-content-between">
-        {renderedPosts}
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
     </div>
+  );
 };
 export default PostList;
