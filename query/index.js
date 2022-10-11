d33971c1867ae8bf7dbd37a0e990564bdef123ed
trancongchien <trancongchien185@gmail.com>
2022-10-11 21:58:29 +0700

    implementing Event Sync

--- query/index.js
+++ query/index.js
@@ -8,12 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const {id, title} = data;
 
@@ -38,13 +33,33 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+};
 
-    console.log(posts);
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    
+
+    handleEvent(type, data);
     
 
     res.send({});
 });
 
-app.listen(4002, () => {
-    console.log('Listening on 4002')
-});
+app.listen(4002, async () => {
+    console.log('Listening on 4002');
+    try {
+        const res = await axios.get("http://localhost:4005/events");
+     
+        for (let event of res.data) {
+          console.log("Processing event:", event.type);
+     
+          handleEvent(event.type, event.data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    });
